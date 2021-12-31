a23633b65f2a54a1f0ef4fc84a02bab020e8bd86
maltia <ghcrowtw@gmail.com>
2021-12-31 14:58:19 +0900

    add: orbitControls

--- src/view/Main/Main.tsx
+++ src/view/Main/Main.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import styles from  './Main.module.css';
 import SampleBox from 'components/SampleBox/SampleBox';
 import { Canvas } from 'react-three-fiber';
- 
+import Controls from 'components/Controls/Controls';
+
 const Main = () => {
   return (
     <div className={styles.main}>
       <Canvas>
         <SampleBox />
+        <Controls />
+        <gridHelper />
       </Canvas>
     </div>
   );
